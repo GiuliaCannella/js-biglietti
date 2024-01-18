bba7d5fd2500e1a279c1089fed81fd0348ad08ca
GiuliaCannella <giucanni99@gmail.com>
2024-01-18 01:19:09 +0100

    add bonus

--- script.js
+++ script.js
@@ -12,18 +12,34 @@
 */
 
 //chiedere eta e km al cliente e converto in numero
-const km =  parseInt(prompt("Inserisci il numero di km da percorrere", 5));
-const eta =  parseInt(prompt("Inserisci la tua età", 20));
+const km = parseInt(prompt("Inserisci il numero di km da percorrere", 5));
+const eta = parseInt(prompt("Inserisci la tua età", 20));
 
 const prezzoKm = 0.21;
 let totalePrezzo = prezzoKm * km;
 
-if (eta < 18) {	
-	totalePrezzo = totalePrezzo - 20 / 100;
-} else if(eta > 65 ) {
-	totalePrezzo = totalePrezzo - 40 /  100;
+if (eta < 18 && eta !== 0) {
+  totalePrezzo = totalePrezzo - 20 / 100;
+} else if (eta > 65) {
+  totalePrezzo = totalePrezzo - 40 / 100;
+}
+totalePrezzo = totalePrezzo.toFixed(2);
+
+const paragrafo = document.getElementById("paragrafo");
+
+let stringaFinale = "";
+if (eta < 18 && eta !== 0) {
+  stringaFinale = `Il prezzo finale del biglietto per Under 18 è: ${totalePrezzo}`;
+} else if (eta > 65) {
+  stringaFinale = `Il prezzo finale del biglietto over 65 è: ${totalePrezzo}`;
+} else if (km === 0) {
+  stringaFinale = `Nessun viaggio in programma per oggi`;
+} else if (isNaN(km) && isNaN(eta)) {
+  stringaFinale = `errore di validazione`;
+}else if( eta === 0 ){
+	stringaFinale = `non è consentito viaggiare ai neonati!`;
+} else {
+  stringaFinale = `Il prezzo finale del biglietto è: ${totalePrezzo}`;
 }
 
-console.log(totalePrezzo.toFixed(2));
-
-const paragrafo = document.getElementById("paragrafo").innerText += totalePrezzo.toFixed(2);
+paragrafo.innerText = stringaFinale;
